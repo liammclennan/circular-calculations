c510c62301132dde2c818c8572817a899e909a9b
Liam McLennan <liam.mclennan@readify.net>
2014-08-24 14:11:25 +1000

    more info

--- Circles/Circles.fs
+++ Circles/Circles.fs
@@ -6,12 +6,15 @@ open System
 // step 1: calculate the angle of each points
 // step 2: the x coordinate will be the cosine of the angle * the radius of the circle
 //         the y coordinate will be the sine of the angle * the radius of the circle
+// more info: http://www.montereyinstitute.org/courses/DevelopmentalMath/COURSE_TEXT2_RESOURCE/U19_L2_T2_text_final.html
+//            http://www.purplemath.com/modules/triggrph.htm  
 let points n r : (float * float) list =
     []
 
 // find the set of intervals between a set of points on a circle
 // step 1: for each pair of adjacent points calculate the euclidean distance between the points ie. sqrt((difference in x values)^2 + (difference in y values)^2))
 // step 2: realise that the intervals are all the same (by definition), therefore the calculation only needs to be done once
+// more info: http://en.wikipedia.org/wiki/Euclidean_distance
 let intervals ps : float list =
     []
 
@@ -20,5 +23,6 @@ let intervalDistance ps : float =
     0.0
 
 // find the ratio (sum of intervals between a set of points on a circle) : (the diameter of the circle)
+// ie. (sum of intervals between adjacent points) / radius * 2
 let ratio r ps : float = 
     0.0
